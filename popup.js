d84f831fe6775a43ecc98562b77e58914bb8c147
Sydney Holubow <sydneyhholubow@gmail.com>
2021-02-26 22:18:35 -0600

    Files uploaded to start

---
+++ popup.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(event){
+	
+	//backend functions here 
+	//how the calculator works/tips page
+
+	window.addEventListener('keydown', keyDown);
+	function keyDown(event) {;
+		    const key = (event.detail || event.which).toString();
+		    if (key == 83) {
+		      calculator.btn.click();
+		      //calculator();
+		    }
+		    else if (key == 70) {
+		      beGreen_btn.click();
+		      //beGreen();
+		    }
+		}
+
+	const calculator_btn = document.getElementById("calculator");
+	calculator_btn.addEventListener('click', function(){
+		//location.href = "https://www.google.com";
+		location.href = "test.html";
+	}, false);
+
+	const beGreen_btn = document.getElementById("beGreen");
+	beGreen.addEventListener('click', function(){
+		location.href = "test.html";
+		//location.href = "https://www.google.com";
+	}, false);
+
+/*
+	function snakeGame() {
+		calculator_btn.click();
+		//window.location.href = "https://www.google.com";
+	}*/
+
+	/*object.onkeydown = function(){calculator()};*/
+
+	/*function keyDown(event) {;
+		    const key = (event.detail || event.which).toString();
+		    if (key == 70) {
+		      beGreem();
+		    }
+		}*/
+
+/*	function beGreen() {
+		beGreen_btn.click();
+		//window.location.href = "https://www.google.com";
+	}*/
+
+	/*object.onkeydown = function(){beGreen()};*/
+}, false);
